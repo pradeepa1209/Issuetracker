77f4894e816190a53bbd76591db9e8d7b5ab2872
Pradeepa Bommuraj <pradeepab129@gmail.com>
2026-07-09 20:11:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,22 @@
 const express=require("express");
 const cors=require("cors");
+require("dotenv").config();
+
 //const Complaint=require("./models/complaint");
 require("./config/db");
 const adminRoutes=require("./routes/adminRoutes");
 const userRoutes=require("./routes/userRoutes");
 const complaintRoutes=require("./routes/complaintRoutes");
+const chatbotRoutes = require("./routes/chatbotRoutes");
+
 const app=express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users",userRoutes);
 app.use("/api/complaints",complaintRoutes);
 app.use("/api/admin",adminRoutes);
+app.use("/api/chatbot", chatbotRoutes);
+
 app.get("/",(req,res)=>{res.send("Server running on port ");
 
 });
